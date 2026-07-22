3eebb503438996ffad37c409a452fc9ad954f0bd
24BCS12627-sketch <24BCS12627@cuchd.in>
2026-07-22 19:47:10 +0530

    Create App.jsx

---
+++ React Projects/Todo-List/App.jsx
@@ -0,0 +1,51 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import { useState } from "react";
+
+function App() {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTask = () => {
+    if (task.trim() === "") return;
+
+    setTodos([...todos, task]);
+    setTask("");
+  };
+
+  const deleteTask = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <>
+      <h1>Todo List</h1>
+
+      <input
+        type="text"
+        placeholder="Enter Task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+
+      <button onClick={addTask}>Add</button>
+
+      <ul>
+        {todos.map((item, index) => (
+          <li key={index}>
+            {item}
+
+            <button onClick={() => deleteTask(index)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
